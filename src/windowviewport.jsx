5882eed693882a22d2a2ce13c8e4cbe44287fbb3
hesam <bhesam2000@yahoo.com>
2023-05-03 20:56:58 +0300

    added comments

--- src/windowviewport.jsx
+++ src/windowviewport.jsx
@@ -1,26 +1,31 @@
 import React, { useRef } from "react";
-import { useMemo, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function RenderList({ Data }) {
 
-    const [visibleItems, setVisibleItems] = useState([]);
-    const scrollRef = useRef(null);
+    const [visibleItems, setVisibleItems] = useState([]); // This Array holds the images the are in viewport
+    const scrollRef = useRef(null); // refrence object for elements Observer
 
     useEffect(() => {
+        // create an observer object that 
+        // changes visible items 
         const observer = new IntersectionObserver(
             (entries) => {
+                // only the visible items are hold 
                 const visible = entries.filter((entry) => entry.isIntersecting).map((entry) => Number(entry.target.dataset.index));
-                // console.log('isvisiblr', visible)
                 setVisibleItems(visible);
             }, { threshold: 0 });
+
+        // list of items in DOM for observing
         const itemsList = scrollRef.current.children[1];
 
+        // iterate through list for observing
         for (let i = 0; i < itemsList.children.length; i++) {
             const item = itemsList.children[i];
             item.dataset.index = i;
             observer.observe(item);
         }
-        // console.log(itemsList.children[5]);
+        // clean up function before re-render
         return () => {
             observer.disconnect();
         };
@@ -46,7 +51,6 @@ function RenderList({ Data }) {
 
         </div>
     )
-
 
 }
 export default RenderList
